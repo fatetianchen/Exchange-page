ba696b885d2f080912855ff2f390dd76dd201ec6
fatetianchen <398272208@qq.com>
2018-04-18 18:38:04 +0800

    first commit

---
+++ js/js.js
@@ -0,0 +1,10 @@
+//var u = navigator.userAgent;
+//if (u.indexOf('Android') > -1 || u.indexOf('Linux') > -1) {//安卓手机
+//	alert("安卓手机");
+//} else if (u.indexOf('iPhone') > -1) {//苹果手机
+//// window.location.href = "mobile/index.html";
+//alert("苹果手机");
+//} else if (u.indexOf('Windows Phone') > -1) {//winphone手机
+//alert("winphone手机");
+//// window.location.href = "mobile/index.html";
+//}
